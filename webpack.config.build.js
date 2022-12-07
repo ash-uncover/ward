938f707755dede7197efbff775dcd64c1baf698b
ash-uncover <ash.uncover@gmail.com>
2022-12-07 09:46:21 +0100

    add bundle

---
+++ webpack.config.build.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+
+const path = require('path')
+
+const DIR_BUNDLE = path.resolve(__dirname, 'bundles')
+const DIR_SRC = path.resolve(__dirname, 'src')
+const DIR_NODE_MODULES = path.resolve(__dirname, 'node_modules')
+
+module.exports = {
+  entry: {
+    'js-utils-microfrontend': path.resolve(DIR_SRC, 'index.ts'),
+  },
+
+  output: {
+    clean: true,
+    path: DIR_BUNDLE,
+    filename: '[name].bundle.js',
+    publicPath: '/',
+  },
+
+  resolve: {
+    modules: ['node_modules', './src'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.(jsx|js)$/,
+        include: DIR_SRC,
+        exclude: DIR_NODE_MODULES,
+        use: [
+          { loader: 'babel-loader' },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        include: DIR_SRC,
+        exclude: DIR_NODE_MODULES,
+        use: [
+          { loader: 'ts-loader' },
+        ],
+      },
+    ],
+  },
+}
