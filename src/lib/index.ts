ebcfce2a4a2baf44be226f1914fca0ff8958b033
ash-uncover <ash.uncover@gmail.com>
2024-05-25 10:45:09 +0200

    correct version number

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -134,7 +134,7 @@ if (!window.uncover?.ward) {
   WardInstance = new WardClass();
   window.uncover = window.uncover || {};
   window.uncover.ward = WardInstance;
-  window.uncover.wardVersion = "0.2.21";
+  window.uncover.wardVersion = "0.2.23";
 } else {
   console.warn(
     `Ward is already registered with version ${window.uncover.wardVersion || "older than 0.2.21"}`
